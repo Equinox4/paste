d4c57ca9dd083cb66c79bffe241cb3a29a20cc49
Mjöllnir <37479575+Equinox4@users.noreply.github.com>
2021-09-15 14:58:01 +0200

    use const

--- create.php
+++ create.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'RedisConnection.php';
+require_once '../RedisConnection.php';
 
 if (empty($_POST['content']) || empty($_POST['duration'])) {
     header('Location: /');
@@ -8,28 +8,28 @@ if (empty($_POST['content']) || empty($_POST['duration'])) {
 
 $redis = RedisConnection::getInstance()->getRedis();
 
-$max_content_len = 100000; // in characters
-$max_custom_id_len = 10;
-$allowed_durations = [ 3600, 7200, 43200, 86400 ]; // in seconds
+const MAX_CONTENT_LEN = 100_000; // in characters
+const MAX_CUSTOM_ID_LEN = 10;
+const ALLOWED_DURATIONS = [ 3_600, 7_200, 43_200, 86_400 ]; // in seconds
 
 $post_content = $_POST['content'];
 $post_duration = $_POST['duration'];
 
-if (strlen($post_content) > $max_content_len) {
+if (strlen($post_content) > MAX_CONTENT_LEN) {
     echo 'Too long content.';
     exit(0);
-}
+}   
 
-if (!in_array($post_duration, $allowed_durations)) {
+if (!in_array($post_duration, ALLOWED_DURATIONS)) {  
     echo 'Invalid duration.';
     exit(0);
-}
+} 
 
 // beurk refaire ça
 
 if (!empty($_POST['custom_id'])) {
     $post_custom_id = $_POST['custom_id'];
-    if (strlen($post_custom_id) > $max_custom_id_len) {
+    if (strlen($post_custom_id) > MAX_CUSTOM_ID_LEN) {
         echo 'Too long id';
         exit(0);
     }
