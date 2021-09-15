34c369266ca8f5e94852d86d448ea7ce2a7c05cc
Mjöllnir <37479575+Equinox4@users.noreply.github.com>
2021-09-15 15:12:05 +0200

    simplification de la gestion des ids

--- create.php
+++ create.php
@@ -6,8 +6,6 @@ if (empty($_POST['content']) || empty($_POST['duration'])) {
     exit(0);
 }
 
-$redis = RedisConnection::getInstance()->getRedis();
-
 const MAX_CONTENT_LEN = 100_000; // in characters
 const MAX_CUSTOM_ID_LEN = 10;
 const ALLOWED_DURATIONS = [ 3_600, 7_200, 43_200, 86_400 ]; // in seconds
@@ -18,48 +16,32 @@ $post_duration = $_POST['duration'];
 if (strlen($post_content) > MAX_CONTENT_LEN) {
     echo 'Too long content.';
     exit(0);
-}   
+}
 
-if (!in_array($post_duration, ALLOWED_DURATIONS)) {  
+if (!in_array($post_duration, ALLOWED_DURATIONS)) {
     echo 'Invalid duration.';
     exit(0);
-} 
+}
 
-// beurk refaire ça
-
-if (!empty($_POST['custom_id'])) {
-    $post_custom_id = $_POST['custom_id'];
-    if (strlen($post_custom_id) > MAX_CUSTOM_ID_LEN) {
+if (empty($_POST['custom_id'])) {
+    $content_id = bin2hex(random_bytes(4));
+}
+else {
+    $content_id = $_POST['custom_id'];
+    if (strlen($content_id) > MAX_CUSTOM_ID_LEN) {
         echo 'Too long id';
         exit(0);
     }
-
-    $can_create = $redis->setNx($post_custom_id, $post_content);
-    if (!$can_create) {
-        echo 'Failed to create paste :(';
-        exit(0);
-    }
-
-    $redis->expire($post_custom_id, $post_duration);
-
-    header("Location: /v/$post_custom_id");
-    exit(0);
 }
 
-$try_count = 1;
-do {
-    $key = bin2hex(random_bytes(4));
-    $can_create = $redis->setNx($key, $post_content);
-
-    $try_count++;
-} while (!$can_create && $try_count <= 3);
-
+$redis = RedisConnection::getInstance()->getRedis();
+$can_create = $redis->setNx($content_id, $post_content);
 if (!$can_create) {
-    echo 'Failed to create paste :( Please retry with an other id.';
+    echo 'Failed to create paste :( Please retry';
     exit(0);
 }
 
-$redis->expire($key, $post_duration);
+$redis->expire($content_id, $post_duration);
 
-header("Location: /v/$key");
+header("Location: /v/$content_id");
 exit(0);
