b53cd3d4b247b871f7bb75a61c6918739b9beb1c
Mjöllnir <37479575+Equinox4@users.noreply.github.com>
2022-09-14 11:53:52 +0200

    remove magic value

--- public/create.php
+++ public/create.php
@@ -6,8 +6,9 @@ if (empty($_POST['content']) || empty($_POST['duration'])) {
     exit(0);
 }
 
-const MAX_CONTENT_LEN = 100_000; // in characters
-const MAX_CUSTOM_ID_LEN = 10; // in characters
+const MAX_CONTENT_LEN      = 100_000; // in characters
+const MAX_CUSTOM_ID_LEN    = 10;      // in characters
+const DEFAULT_RAND_ID_LEN  = 5;       // in bytes
 const ALLOWED_DURATIONS = [ 3_600, 7_200, 43_200, 86_400 ]; // in seconds
 
 $post_content = $_POST['content'];
@@ -24,7 +25,7 @@ if (!in_array($post_duration, ALLOWED_DURATIONS)) {
 }
 
 if (empty($_POST['custom_id'])) {
-    $content_id = bin2hex(random_bytes(5));
+    $content_id = bin2hex(random_bytes(DEFAULT_RAND_ID_LEN));
 }
 else {
     $content_id = $_POST['custom_id'];
